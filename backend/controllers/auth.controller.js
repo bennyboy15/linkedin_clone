3e23c99aa9c8abfb90c50e73d86477fd880302bf
BenjaminHarveyCMVTC <benjamin.harvey@cmvtruckcentre.com.au>
2025-10-16 15:18:49 +1030

    Login, logout & protect routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -66,10 +66,54 @@ export async function signup(req,res){
     }
 };
 
-export function login(req,res){
-    console.log("login!")
+export async function login(req,res){
+    try {
+        const {username, password} = req.body;
+        
+        // MISSING FIELDS 
+        if(!username || !password) {
+            return res.status(400).json({success:false, message:"All fields are required"});
+        };
+
+        // USER EXISTS?
+        const existingUser = await User.findOne({username});
+        if(!existingUser) {
+            return res.status(400).json({success:false, message:"Invalid credentials"});
+        };
+
+        // PASSWORD CORRECT?
+        const isMatch = await bcrypt.compare(password, existingUser.password);
+        if (!isMatch) {
+            return res.status(400).json({success:false, message:"Invalid credentials"});
+        };
+
+        // GENERATE TOKEN
+        const token = jwt.sign({userId: existingUser._id}, process.env.JWT_SECRET, {expiresIn: "3d"});
+        await res.cookie("jwt-linkedin", token, {
+            httpOnly: true,
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        res.json({message: "Logged in successfully"});
+
+    } catch (error) {
+        console.log("Error in login:", error.message);
+        res.status(500).json({success:false, message: "Internal Server Error"});
+    }
 };
 
 export function logout(req,res){
-    console.log("logout!")
+    res.clearCookie("jwt-linkedin");
+    res.json({success: true, message: "Logged out successfully"});
 };
+
+export async function getCurrentUser(req,res) {
+    try {
+        res.json(req.user)
+    } catch (error) {
+        console.log("Error in getCurrentUser controller:", error.message);
+        res.status(500).json({success:false, message: "Internal Server Error"});
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import {connectDB} from "./lib/db.js"
+import cookieParser from "cookie-parser";
 
 const app = express();
 config();
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/v1/auth", authRoutes);
